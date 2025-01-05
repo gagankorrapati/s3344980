66709d3462faae466ad2a49116528e073937e587
gagankorrapati <s3344980@live.tees.ac.uk>
2025-01-05 15:56:25 +0000

    petitemDB added

--- petcat/app/src/main/java/uk/ac/tees/mad/univid/data/local/PetDatabase.kt
+++
@@ -1,9 +0,0 @@
-package uk.ac.tees.mad.univid.data.local
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [PetItemDB::class], version = 1, exportSchema = false)
-abstract class PetDatabase : RoomDatabase() {
-    abstract fun petDao(): PetDao
-}
--- petcat/app/src/main/java/uk/ac/tees/mad/univid/di/MainApplication.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.univid.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MainApplication : Application() {
-}
